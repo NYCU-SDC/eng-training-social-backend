a0684f55ef11768e9b6f25da90bb2f14337c820f
Kyle9410-Chen <kyle941010@gmail.com>
2025-08-14 16:33:59 +0800

    fix: rename user_id to author_id in comments table for clarity

--- internal/database/migrations/3_comments.up.sql
+++ internal/database/migrations/3_comments.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS comments (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     post_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
     author_id UUID REFERENCES users(id) ON DELETE CASCADE,
-    content TEXT,
+    content TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT now(),
     updated_at TIMESTAMPTZ DEFAULT now()
 );

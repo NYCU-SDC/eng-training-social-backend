cde5a2e14c497a595d8b526c85ffdcbb250a68fa
Kyle9410-Chen <kyle941010@gmail.com>
2025-08-19 16:24:02 +0800

    feat: integrate reaction handling for posts and comments with user context

--- internal/reaction/service.go
+++ internal/reaction/service.go
@@ -44,14 +44,28 @@ func (s *Service) RemoveToPost(ctx context.Context, postID, userID uuid.UUID) er
 	return nil
 }
 
-func (s *Service) GetByPostIDAndUserID(ctx context.Context, postID, userID uuid.UUID) (Reaction, error) {
+func (s *Service) GetByPostIDAndUserID(ctx context.Context, postID, userID uuid.UUID) (ReactionType, error) {
+	exists, err := s.queries.ExistsByPostIDAndUserID(ctx, ExistsByPostIDAndUserIDParams{
+		PostID: pgtype.UUID{Valid: true, Bytes: postID},
+		UserID: userID,
+	})
+	if err != nil {
+		s.logger.Error("Failed to get reaction", zap.Error(err))
+		return ReactionTypeNONE, err
+	}
+
+	if !exists {
+		s.logger.Info("No reaction found for post", zap.String("postID", postID.String()), zap.String("userID", userID.String()))
+		return ReactionTypeNONE, nil
+	}
+
 	reaction, err := s.queries.GetByPostIDAndUserID(ctx, GetByPostIDAndUserIDParams{
 		PostID: pgtype.UUID{Valid: true, Bytes: postID},
 		UserID: userID,
 	})
 	if err != nil {
 		s.logger.Error("Failed to get reactions by post ID", zap.Error(err))
-		return Reaction{}, err
+		return ReactionTypeNONE, err
 	}
 	return reaction, nil
 }
@@ -81,14 +95,28 @@ func (s *Service) RemoveToComment(ctx context.Context, commentID, userID uuid.UU
 	return nil
 }
 
-func (s *Service) GetByCommentIDAndUserID(ctx context.Context, commentID, userID uuid.UUID) (Reaction, error) {
+func (s *Service) GetByCommentIDAndUserID(ctx context.Context, commentID, userID uuid.UUID) (ReactionType, error) {
+	exists, err := s.queries.ExistsByCommentIDAndUserID(ctx, ExistsByCommentIDAndUserIDParams{
+		CommentID: pgtype.UUID{Valid: true, Bytes: commentID},
+		UserID:    userID,
+	})
+	if err != nil {
+		s.logger.Error("Failed to check existence of reaction", zap.Error(err))
+		return ReactionTypeNONE, err
+	}
+
+	if !exists {
+		s.logger.Info("No reaction found for comment", zap.String("commentID", commentID.String()), zap.String("userID", userID.String()))
+		return ReactionTypeNONE, nil
+	}
+
 	reaction, err := s.queries.GetByCommentIDAndUserID(ctx, GetByCommentIDAndUserIDParams{
 		CommentID: pgtype.UUID{Valid: true, Bytes: commentID},
 		UserID:    userID,
 	})
 	if err != nil {
 		s.logger.Error("Failed to get reactions by comment ID", zap.Error(err))
-		return Reaction{}, err
+		return ReactionTypeNONE, err
 	}
 	return reaction, nil
 }

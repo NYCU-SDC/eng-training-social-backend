7626264cd2458ad73c344bfc7f29f90916a61b95
Kyle9410-Chen <kyle941010@gmail.com>
2025-08-21 15:51:11 +0800

    refactor: replace local getUserFromContext function with jwt package method

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"context"
-	"errors"
 	"github.com/NYCU-SDC/eng-training-social-backend/internal"
 	"github.com/NYCU-SDC/eng-training-social-backend/internal/follow"
 	"github.com/NYCU-SDC/eng-training-social-backend/internal/jwt"
